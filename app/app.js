6fc4c10d57259275836787b9be134845a89086e9
SampathFG <sampath@interns.fortunaglobal.com>
2016-06-22 14:10:49 +0530

    initial

---
+++ app/app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Tarun on 6/17/2016.
+ */
+var app = angular.module('propozerApp', []); //'ngRoute','ui.bootstrap',''ngAnimate'
+
+
+
+// app.config(function($RouterProvide){
+//     $RouterProvide.when('/',{
+//         controller:'indexCtrl',
+//         templateUrl:'index.html'
+//     }
+//
+//     )
+//
+// });
---
+++ controller.js
@@ -0,0 +1,17 @@
+app.controller('HeaderCtrl', ['$scope', function($scope) {
+    $scope.name="Tarun";
+    $scope.showLogInUser = function(){
+        alert("hiii");
+//TODO
+ //        $uibModal.open({
+ //     templateUrl:'template/partial/logInPopUp.html',
+ //     controller:'logInPopUpCtrl'
+ // });
+    }
+    }]);
+
+
+// app.controller('logInPopUpCtrl', [ '$scope', function($scope) {
+//
+// }]);
+
